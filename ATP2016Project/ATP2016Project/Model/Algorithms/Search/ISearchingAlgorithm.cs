08f7bf13eb2ca9452b20d9a13f51c431f30a0583
shahar3 <shahar061@gmail.com>
2016-05-04 16:58:13 +0100

    comment!!

--- ATP2016Project/ATP2016Project/Model/Algorithms/Search/ISearchingAlgorithm.cs
+++ ATP2016Project/ATP2016Project/Model/Algorithms/Search/ISearchingAlgorithm.cs
@@ -6,10 +6,30 @@ using System.Threading.Tasks;
 
 namespace ATP2016Project.Model.Algorithms.Search
 {
+    /// <summary>
+    /// the basic interface for any searching algorithm
+    /// includes the main function search that solve the searching problem
+    /// and 2 helping methods to determine the efficiency level of each
+    /// algorithm
+    /// </summary>
     interface ISearchingAlgorithm
     {
+        /// <summary>
+        /// in this function we implement the search algorithm on a general searching problem
+        /// </summary>
+        /// <param name="searchable">can vary through many different searching problem</param>
+        /// <returns>the solution for the problem</returns>
         Solution search(ISearchable searchable);
+        /// <summary>
+        /// help us to determine if the algorithm is effective compared to other algorithms
+        /// </summary>
+        /// <returns>how many states was developed during the search process</returns>
         int statesDeveloped();
+        /// <summary>
+        /// help us to determine how good is the algorithm time-wise.
+        /// </summary>
+        /// <param name="searchable"></param>
+        /// <returns>the time elapsed since we start to solve the problem untill it solved</returns>
         String timeToSolve(ISearchable searchable);
     }
 }
