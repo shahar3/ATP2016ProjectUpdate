4ecb749f9f25c98ba25d06658f497737d0ffeba9
shahar3 <shahar061@gmail.com>
2016-05-24 13:18:02 +0300

    comments

--- ATP2016Project/ATP2016Project/Controller/IController.cs
+++ ATP2016Project/ATP2016Project/Controller/IController.cs
@@ -10,12 +10,25 @@ namespace ATP2016Project.Controller
 {
     /// <summary>
     /// the facade of the contoroller layer
+    /// interacts with the model layer and the view layer
+    /// in our MVC design pattern
     /// </summary>
     interface IController
     {
+        /// <summary>
+        /// set the model layer to be the object we get
+        /// </summary>
+        /// <param name="model">the model object</param>
         void SetModel(IModel model);
+        /// <summary>
+        /// set the view layer to be the object we get
+        /// </summary>
+        /// <param name="view">the view object</param>
         void SetView(IView view);
-        void Output(string output);
+        /// <summary>
+        /// get the commands for our CLI that we set in our dictionary
+        /// </summary>
+        /// <returns>the dictionary initialized with the commands</returns>
         Dictionary<string, ICommand> GetCommands();
     }
 }
