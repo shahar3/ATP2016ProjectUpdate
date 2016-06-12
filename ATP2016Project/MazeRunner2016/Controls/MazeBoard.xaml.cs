276c205dd4dccdbe57b5717c40133c72ee9cfe77
shahar3 <shahar061@gmail.com>
2016-06-12 19:43:57 +0300

    controlsAdded

---
+++ ATP2016Project/MazeRunner2016/Controls/MazeBoard.xaml.cs
@@ -0,0 +1,49 @@
+﻿using MazeLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MazeRunner2016.Controls
+{
+    /// <summary>
+    /// Interaction logic for MazeBoard.xaml
+    /// </summary>
+    public partial class MazeBoard : UserControl
+    {
+        private IView view;
+        private IModel model;
+        private Maze3d myMaze;
+
+        public MazeBoard()
+        {
+            InitializeComponent();
+        }
+
+        public MazeBoard(IView view, IModel model, Object mazeObject)
+        {
+            this.view = view;
+            this.model = model;
+            //get the maze dimensions
+            myMaze = mazeObject as Maze3d;
+            int x = (myMaze.Maze2DLayers[0] as Maze2d).Grid.GetLength(0);
+            int y = (myMaze.Maze2DLayers[0] as Maze2d).Grid.GetLength(1);
+            int z = myMaze.ZLength;
+            //maxLevel = z;
+            //string msg = string.Format("The maze dimenstions are x:{0} y:{1} z:{2}", x, y, z);
+            //MessageBox.Show(msg);
+            //createGrid(x, y, 0);
+            //initializeGrid(myMaze, levelsGrid[0], 0, true);
+        }
+    }
+}
---
+++ ATP2016Project/MazeRunner2016/Controls/PlayerControl.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MazeRunner2016.Controls
+{
+    /// <summary>
+    /// Interaction logic for PlayerControl.xaml
+    /// </summary>
+    public partial class PlayerControl : UserControl
+    {
+        public PlayerControl()
+        {
+            InitializeComponent();
+        }
+
+        public PlayerControl(int width, int height)
+        {
+            InitializeComponent();
+            player.Width = width;
+            player.Height = height;
+        }
+    }
+}
