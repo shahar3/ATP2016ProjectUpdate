f608b7e844f7e7231f1c2d8426e858c319eaee56
shahar3 <shahar061@gmail.com>
2016-06-19 19:08:09 +0300

    com

--- ATP2016Project/MazeRunner2016/Model/IModel.cs
+++ ATP2016Project/MazeRunner2016/Model/IModel.cs
@@ -7,10 +7,16 @@ using System.Threading.Tasks;
 
 namespace MazeRunner2016
 {
+    //this the delegate that define in the event model change will see later
     public delegate void finishedComputing(string name, string otherInformation);
 
+    /// <summary>
+    /// this the interfacr of the model
+    /// here we declare about all th functions that apper in the model
+    /// </summary>
     public interface IModel
     {
+
         event finishedComputing ModelChanged;
 
         void generateMaze(int x, int y, int z, string mazeName);
